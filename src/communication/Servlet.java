8c88de27d84d617f6f6ce9fefde58ee4e1cea71b
mdjfs <marcos.fuenmayorhtc@gmail.com>
2020-02-01 18:09:10 -0400

    Upload the servlet with Hello World

---
+++ src/communication/Servlet.java
@@ -0,0 +1,23 @@
+package communication;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet{
+	// mdjfs: declaration of this servlet in WebContent/WEB-INF/web.xml
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// mdjfs: returns Hello World ! on servlet with method GET
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.println("<h1> Hello World ! </h1>");
+			out.close();
+		
+	}
+
+}
