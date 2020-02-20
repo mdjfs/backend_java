3513606bc57d56d45178e70104b6fe04cf4566b4
mdjfs <marcos.fuenmayorhtc@gmail.com>
2020-02-19 22:10:22 -0400

    Add the DBComponent and ConfigComponent

---
+++ src/bussinessObjects/DBComponent.java
@@ -0,0 +1,30 @@
+package bussinessObjects;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBComponent {
+	
+	private Connection conn = null;
+	private ConfigComponent config_db = new ConfigComponent("/home/mdjfs/Documentos/db_config.properties");
+	private Properties db_properties = null;
+	
+	public DBComponent() 
+	{
+		try 
+		{
+			db_properties = config_db.getObjectProperties();
+			Class.forName(db_properties.getProperty("db.driver"));
+			this.conn = DriverManager.getConnection(db_properties.getProperty("db.url")
+													,db_properties.getProperty("db.username")
+													,db_properties.getProperty("db.password"));
+		} 
+		catch (SQLException | ClassNotFoundException sqle) 
+		{
+	            System.out.println("Error connecting in SQL: " + sqle);
+		}
+	}
+	
+}
